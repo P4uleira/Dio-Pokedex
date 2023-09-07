160e973ee5af9510068f36d93ced9506e1209527
P4uleira <pricardoporfirio2001@gmail.com>
2023-09-07 15:25:49 -0300

    Alterando background dinamicamente

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,8 +1,29 @@
+
 const pokeApi = {}
 
-pokeApi.getPokemonDetail = (pokemon) => {
-   return fetch(pokemon.url).then((response) => response.json())
+function convertPokeApiDetailsToPokemon(pokeDetails){
+    const pokemon = new Pokemon()
+
+    pokemon.number = pokeDetails.order
+    pokemon.name = pokeDetails.name
+
+    const types = pokeDetails.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetails.sprites.other.dream_world.front_default
+
+    return pokemon
 }
+
+pokeApi.getPokemonDetail = (pokemon) => {
+   return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailsToPokemon)
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
